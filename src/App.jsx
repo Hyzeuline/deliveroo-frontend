d3c34b55cc6af706ca2a2eaf97cec612ab0943cc
hyzeuline <iseline.voison@gmail.com>
2025-08-25 15:57:06 +0200

    create front

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import "./App.css";
 import logo from "../public/logo.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("déclenchement useEffect");
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/");
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <p>Chargement ...</p>
+  ) : (
     <>
+      {data.map((element, index) => {
+        return <p key={index}>{element}</p>;
+      })}
       <header>
         <img src={logo} alt="logo-deliveroo" />
       </header>
+      <main></main>
     </>
   );
 }
